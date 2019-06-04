5d54d3f6b103d0b8014df828451b5197056dc27f
Marko Kuosmanen <marko.kuosmanen@sitowise.com>
2019-06-04 14:45:51 +0300

    Fixed bundle register

--- server-extension/src/main/java/flyway/valkeakoski/V1_0_6__register_track_location_bundle.java
+++ server-extension/src/main/java/flyway/valkeakoski/V1_0_6__register_track_location_bundle.java
@@ -1,19 +1,19 @@
 package flyway.valkeakoski;
 
 import fi.nls.oskari.db.BundleHelper;
-import fi.nls.oskari.db.BundleHelper_pre1_52;
 import fi.nls.oskari.domain.map.view.Bundle;
 import org.flywaydb.core.api.migration.jdbc.JdbcMigration;
 
 import java.sql.Connection;
+import java.sql.SQLException;
 
 public class V1_0_6__register_track_location_bundle implements JdbcMigration {
     private static final String BUNDLE_ID = "track-location";
 
-    public void migrate(Connection connection) {
+    public void migrate(Connection connection)  throws SQLException {
         // BundleHelper checks if these bundles are already registered
         Bundle bundle = new Bundle();
         bundle.setName(BUNDLE_ID);
-        BundleHelper.registerBundle(bundle);
+        BundleHelper.registerBundle(bundle, connection);
     }
 }
